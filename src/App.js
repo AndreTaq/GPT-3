5e5215771235ac189a75765fcc410be0dc8ca11b
AndreTaq <andretaqian@gmail.com>
2024-08-10 20:01:21 -0700

    created components files and containers files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import './App.css';
+
+// we can import all the files togther from index
+import { Brand, Cta, Navbar } from './components'
+import { Blog, Features, Footer, Header, Possibility, WhatGPT3 } from './containers'
+
 
 const App = () => {
   return (
-    <div>
-      <h1>GPT-3</h1>
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <Cta />
+      <Blog />
+      <Footer />
     </div>
   )
 }
